58d3c83885de6dbc56d476141b3e81436b4128d5
selunders <sethlunders@gmail.com>
2024-02-07 22:44:12 -0800

    Test ssr

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <h1>Hello Github Pages!</h1>
       <Counter />
       <p>
         Visit{" "}
